9ea1afbbabf9f099989ec458c34436e6a9f21aa9
sohansamant <sohansamant@outlook.com>
2016-09-19 00:56:13 -0500

    Create Create_Directory.java

---
+++ Create_Directory.java
@@ -0,0 +1,23 @@
+import java.io.File;
+ 
+public class Create_Directory
+{
+    public static void main(String[] args)
+    {	
+	File file = new File("C:\\Directory1");
+	if (!file.exists()) {
+		if (file.mkdir()) {
+			System.out.println("Directory is created!");
+                        
+		} 
+       
+	}
+         else
+                {
+                    System.out.println("Directory All ready exi");
+                }
+ 
+	
+	}
+ 
+    }
